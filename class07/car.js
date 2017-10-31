cad10e2826a9a6233166f56e0aafbb2714d94aa6
martypdx <marty@martypdx.com>
2017-10-31 10:27:03 -0700

    Add render and a separate Bid constructor function

--- class07/car.js
+++ class07/car.js
@@ -1,33 +1,49 @@
 
-// constructor function
 function Car(make, model, year, color) {
-    // initialization logic
-    
     this.make = make;
     this.model = model;
     this.year =  year;
     this.color = color;
-
-    // this isn't a passed-in value,
-    // but a value we need for this "class"
-    // to do it's work
     this.bids = [];
 }
 
 Car.prototype.render = function() {
-        
-};
-    
-Car.prototype.addBid = function() {
+    const div = document.createElement('DIV');
 
+    const h2 = document.createElement('H2');
+    h2.textContent = this.make + ' '  + this.model;
+    div.appendChild(h2);
+
+    const ul = document.createElement('UL');      
+
+    for(let i = 0; i < this.bids.length; i++) {
+        ul.appendChild(this.bids[i].render());
+    }
+    div.appendChild(ul);
+
+    return div;
+};
+
+Car.prototype.addBid = function(name, amount) {
+    const bid = new Bid(name, amount);
+    this.bids.push(bid);
+};
+
+function Bid(name, amount) {
+    this.name = name;
+    this.amount = amount;
+}
+
+Bid.prototype.render = function () {
+    const li = document.createElement('LI');
+    li.textContent = this.name + ' bids ' + this.amount;
+    return li;
 };
 
 const pinto = new Car('Ford', 'Pinto', 1979, 'limegreen');
-const fiat = new Car('Fiat', '500', 2015, 'red');
+// const fiat = new Car('Fiat', '500', 2015, 'red');
 
-console.log(pinto, fiat);
+pinto.addBid('marty', 2.00);
+pinto.addBid('joey', 3.00);
 
-console.log('same proto parent?',
-    Object.getPrototypeOf(pinto) ===
-    Object.getPrototypeOf(fiat)
-);
+document.body.appendChild(pinto.render());
